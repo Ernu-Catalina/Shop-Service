8d1146cd5f2c2413e7a8e29ae698d1df3176ff2d
Catalina Ernu <catalina.ernu@gmail.com>
2025-10-23 22:48:49 +0300

    made some changes

--- src/ShopApi/obj/Debug/net9.0/ShopApi.AssemblyInfo.cs
+++ src/ShopApi/obj/Debug/net9.0/ShopApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da65748d9791e4e81f145384120930987e7048f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2feab1c863658581c0aaaacb3d6f2071d9c72f95")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
